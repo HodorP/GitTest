7951568727dc1efc8ad936d488616fa526a8f779
hodor <hagberry_am@list.ru>
2024-06-19 02:02:20 +0400

    workflow_dispatch: мануальные тесты с выбором

--- tests/test_button.py
+++ tests/test_button.py
@@ -15,3 +15,8 @@ def driver(request):
 def test_button_exist(driver):
     driver.get("https://www.qa-practice.com/elements/button/simple")
     assert driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+
+
+def test_like_a_button_exist(driver):
+    driver.get("https://www.qa-practice.com/elements/button/like_a_button#")
+    assert driver.find_element(By.CSS_SELECTOR, ".a-button")
